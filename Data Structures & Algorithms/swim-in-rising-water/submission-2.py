a28be635361951611b595c0d93564856216f39f5
Bruce Xu <brucexu@sas.upenn.edu>
2026-04-19 21:57:10 +0800

    Add: swim-in-rising-water - submission-2

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-2.py
@@ -0,0 +1,26 @@
+import heapq
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+    
+        row, col = len(grid), len(grid[0])
+        # (h, r, c)
+        arr = [(0, 0, 0)]
+        visit = {(0, 0): grid[0][0]}
+        heap = set()
+        heapq.heapify(arr)
+
+        while arr:
+            h, r, c = heapq.heappop(arr)
+            if (r, c) in heap:
+                continue
+            for nr, nc in [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]:
+                if nr == row - 1 and nc == col - 1:
+                    return max(visit[(r, c)], grid[nr][nc])
+                if min(nr, nc) < 0 or nr == row or nc == col or (nr, nc) in heap:
+                    continue
+                visit[(nr, nc)] = max(visit[(r, c)], grid[nr][nc])
+                heapq.heappush(arr, (grid[nr][nc], nr, nc))
+            heap.add((r, c))
+        
+                
+
