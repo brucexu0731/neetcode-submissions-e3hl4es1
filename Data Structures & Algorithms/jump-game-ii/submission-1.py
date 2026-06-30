9c55d9adaa36cbae6ae91146cbf4e23c30d498eb
Bruce Xu <brucexu@sas.upenn.edu>
2026-06-30 16:30:14 +0800

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,26 @@
+from collections import deque
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        if len(nums) == 1:
+            return 0
+        queue = deque()
+
+        queue.append(0)
+
+        depth = 1
+        prev = 0
+        while queue:
+            print(queue)
+            furthest = prev
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                furthest = max(furthest, curr + nums[curr])
+                if furthest >= len(nums) - 1:
+                    return depth 
+            for i in range(prev + 1, furthest + 1):
+                queue.append(i)
+            prev = furthest 
+            depth += 1
+        
+        return -1
