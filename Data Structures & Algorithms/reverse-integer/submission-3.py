c76472bc6ac2283bc30c882594a0676903e8b3dd
Bruce Xu <brucexu@sas.upenn.edu>
2026-07-01 02:23:37 +0800

    Add: reverse-integer - submission-3

---
+++ Data Structures & Algorithms/reverse-integer/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0 or abs(x) > 9999999999:
+            return 0
+        res = "" if x > 0 else "-"
+        x = abs(x)
+        while x:
+            curr = x % 10
+            res += str(curr)
+            x //= 10
+
+        
+        if -2 ** 31 <= int(res) <= 2 ** 31 - 1:
+            return int(res)
+        else:
+            return 0
+        
