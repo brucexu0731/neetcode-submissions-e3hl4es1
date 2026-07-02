3efb8522b826d3a04ce91af41c2f4068944fc33b
Bruce Xu <brucexu@sas.upenn.edu>
2026-07-02 15:23:30 +0800

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        # can do dfs with backtracking, track the path with a stack
+        # if encountering a *, then split into 3 branches 
+        # 3^n 
+        flex = []
+        open_paren = []
+
+        for i in range(len(s)):
+            if s[i] == ")":
+                if not open_paren and not flex:
+                    return False
+                elif not open_paren:
+                    flex.pop()
+                else:
+                    open_paren.pop()
+            
+            if s[i] == "(":
+                open_paren.append(i)
+            
+            if s[i] == "*":
+                flex.append(i)
+
+        while open_paren:
+            if not flex:
+                return False
+            if open_paren[-1] > flex[-1]:
+                return False
+            open_paren.pop()
+            flex.pop()
+        
+        return True
